b749bcd5df4e88f225aa30c12c73a08885b62cb7
Jelrik van Hal <jelrik@presentblue.nl>
2021-02-11 11:03:48 +0100

    Implements the TodoList

--- src/Domain/TodoList.php
+++ src/Domain/TodoList.php
@@ -9,9 +9,26 @@ final class TodoList
     /** @var array<Item> */
     private array $items = [];
 
+    public function __construct(Item ...$items)
+    {
+        $this->items = $items;
+    }
+
     /** @return array<Item> */
     public function items(): array
     {
         return $this->items;
+    }
+
+    public function add(Item $item): void
+    {
+        $this->items[] = $item;
+    }
+
+    public function remove(Item $itemToRemove): void
+    {
+        $this->items = array_values(array_filter($this->items, function ($itemToInspect) use ($itemToRemove) {
+            return ($itemToRemove !== $itemToInspect);
+        }));
     }
 }
--- tests/Domain/TodoListTest.php
+++ tests/Domain/TodoListTest.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+namespace Todo\Domain;
+
 use PHPUnit\Framework\TestCase;
-use Todo\Domain\TodoList;
 
 /**
  * @covers \Todo\Domain\TodoList
@@ -14,5 +15,40 @@ final class TodoListTest extends TestCase
     {
         $todoList = new TodoList();
         $this->assertSame([], $todoList->items());
+    }
+
+    public function test_a_todo_list_can_be_created_with_items_in_it(): void
+    {
+        $item1 = new Item();
+        $item2 = new Item();
+
+        $todoList = new TodoList($item1, $item2);
+
+        $this->assertSame([$item1, $item2], $todoList->items());
+    }
+
+    public function test_we_can_add_items_to_a_todo_list(): void
+    {
+        $todoList = new TodoList();
+        $item1 = new Item();
+        $item2 = new Item();
+
+        $todoList->add($item1);
+        $todoList->add($item2);
+
+        $this->assertSame([$item1, $item2], $todoList->items());
+    }
+
+    public function test_we_can_remove_an_item_from_a_todo_list(): void
+    {
+        $todoList = new TodoList();
+        $item1 = new Item();
+        $item2 = new Item();
+        $todoList->add($item1);
+        $todoList->add($item2);
+
+        $todoList->remove($item1);
+
+        $this->assertSame([$item2], $todoList->items());
     }
 }
